c98771f429f4c9addbf3fe5e6ab2e2562d30a277
Dmytro1001 <31dmitri13@gmail.com>
2023-05-17 20:39:29 +0300

    fixed message and httperror

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -3,10 +3,13 @@ const { HttpError } = require("../helpers");
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
-    if (error) {
+    if (Object.keys(req.body).length === 0) {
+      next(HttpError(400, "missing fields"));
+    } else if (error) {
       next(HttpError(400, error.message));
+    } else {
+      next();
     }
-    next();
   };
   return func;
 };
--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -3,16 +3,19 @@ const Joi = require("joi");
 const phonePattern = /^\(\d{3}\) \d{3}-\d{4}$/;
 
 const contactAddSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().required().email().messages({
-    "string.email": "Must be a valid email address",
-    "any.required": "Email is required",
-  }),
-  phone: Joi.string().pattern(phonePattern).required().messages({
-    "string.pattern.base":
-      "Incorrect phone number format. Example: (099) 111-1111",
-    "any.required": "Phone number is required",
-  }),
+  name: Joi.string()
+    .min(3)
+    .max(20)
+    .required()
+    .messages({ "any.required": "missing required name field" }),
+  email: Joi.string()
+    .required()
+    .email()
+    .messages({ "any.required": "missing required email field" }),
+  phone: Joi.string()
+    .pattern(phonePattern)
+    .required()
+    .messages({ "any.required": "missing required phone field" }),
 });
 
 module.exports = { contactAddSchema };
